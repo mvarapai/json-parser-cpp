e429e2a4753b34706fc91f00c8079e7f5ecbbdab
Mikalai Varapai <mikalaivarapai@gmail.com>
2024-10-28 15:08:34 +0100

    Intermediate version

--- src/include/json_parser.h
+++ src/include/json_parser.h
@@ -9,95 +9,157 @@
 #include <string>
 #include <unordered_map>
 
+class JSONSource;
+class JSONString;
+
+#define SYNTAX_MSG_TYPE_ERROR 0
+#define SYNTAX_MSG_TYPE_WARNING 1
+#define SYNTAX_MSG_TYPE_MESSAGE 2
+
+// Class to represent JSON syntax tree.
 class JSON
 {
 private:
-	// Due to several ways to intialize a JSON,
-	// contructor will be private and empty.
-	JSON() { }
+    // Create JSON from file
+    JSON(const std::string& filename);
 
 public:
-	JSON& operator=(const JSON& rhs) = default;
-	JSON(const JSON& other) { }
+    // Default copy and assignment
+    JSON& operator=(const JSON& rhs) = delete;
+    JSON(const JSON& other) = delete;
 
 public:
-	static JSON ReadFromFile(const std::string& filename);
-	static JSON ReadFromString(std::string source);
+    enum class JSON_NODE_TYPE
+    {
+        JSON_NODE_TYPE_INVALID = -1,
+        JSON_NODE_TYPE_OBJECT = 0,
+        JSON_NODE_TYPE_LIST = 1,
+        JSON_NODE_TYPE_LITERAL_STRING = 2,
+        JSON_NODE_TYPE_LITERAL_INT = 3,
+    };
 
-public:
-	enum class JSON_NODE_TYPE
-	{
-		JSON_NODE_TYPE_INVALID = -1,
-		JSON_NODE_TYPE_OBJECT = 0,
-		JSON_NODE_TYPE_LIST = 1,
-		JSON_NODE_TYPE_LITERAL_STRING = 2,
-		JSON_NODE_TYPE_LITERAL_INT = 3,
-	};
+    bool isLiteral(JSON_NODE_TYPE type) { return (int)type > 1; }
 
-	bool isLiteral(JSON_NODE_TYPE type) { return (int)type > 1; }
+    // Basic node object. Contains only type, cannot be instantiated.
+    class JSONNode
+    {
+        // It is expected that type of the node cannot be changed during runtime.
+        const JSON_NODE_TYPE type = JSON_NODE_TYPE::JSON_NODE_TYPE_INVALID;
 
-	// Basic node object. Contains only type, cannot be instantiated.
-	class JSONNode
-	{
-		// It is expected that type of the node cannot be changed during runtime.
-		const JSON_NODE_TYPE type = JSON_NODE_TYPE::JSON_NODE_TYPE_INVALID;
+    public:
+        JSONNode(JSON_NODE_TYPE nodeType) : type(nodeType) { }
+    };
 
-	public:
-		JSONNode(JSON_NODE_TYPE nodeType) : type(nodeType) { }
-	};
+    // JSON object - contains a list of identifiers and links further down the tree.
+    struct JSONObject : JSONNode
+    {
+        JSONObject() : JSONNode(JSON_NODE_TYPE::JSON_NODE_TYPE_OBJECT) { }
+        std::unordered_map<std::string, JSONNode*> members;
+    };
 
-	// JSON object - contains a list of identifiers and links further down the tree.
-	struct JSONObject : JSONNode
-	{
-		JSONObject() : JSONNode(JSON_NODE_TYPE::JSON_NODE_TYPE_OBJECT) { }
-		std::unordered_map<std::string, JSONNode*> members;
-	};
+    //	List - special structure in the tree, works parallel to JSONObject.
+    struct JSONList : public JSONNode
+    {
+        JSONList() : JSONNode(JSON_NODE_TYPE::JSON_NODE_TYPE_LIST) { }
+        std::vector<JSONObject*> elements;
+    };
 
-	//	List - special structure in the tree, works parallel to JSONObject.
-	struct JSONList : public JSONNode
-	{
-		JSONList() : JSONNode(JSON_NODE_TYPE::JSON_NODE_TYPE_LIST) { }
-		std::vector<JSONObject*> elements;
-	};
+    // JSON literal - leaf of the tree.
+    template <typename T>
+    struct JSONLiteral : public JSONNode
+    {
+    private:
+        T value;
 
-	// JSON literal - leaf of the tree.
-	template <typename T>
-	struct JSONLiteral : public JSONNode
-	{
-	private:
-		T value;
+    public:
+        JSONLiteral(T literalValue, JSON_NODE_TYPE literalType) : 
+            value(literalValue), JSONNode(literalType) { }
 
-	public:
-		JSONLiteral(T literalValue, JSON_NODE_TYPE literalType) : 
-			value(literalValue), JSONNode(literalType) { }
-
-		T GetValue() { return value; }
-		JSON_NODE_TYPE GetType() { return type; }
-	};
+        T GetValue() { return value; }
+        JSON_NODE_TYPE GetType() { return type; }
+    };
 
 
 private:
-	// Root of the JSON sytax tree, must be a JSON object.
-	JSONObject* globalSpace = nullptr;
+    // Root of the JSON sytax tree, must be a JSON object.
+    JSONObject* globalSpace = nullptr;
+    JSONSource* jsonSource = nullptr;
+
+    void Release()
+    {
+        // Go down the syntax tree and call Release on these nodes
+    }
+
+    ~JSON()
+    {
+        // TODO: Tree deallocation
+        delete jsonSource;
+    }
 };
 
 
 // Class for dealing with JSON syntax error highlighing
 class JSONSource
 {
-	std::string sourceStr;
-	std::string trimmedStr;
+    std::string sourceStr;	// String as in initial JSON file
+    std::string trimmedStr;	// String without whilespaces, newlines and tabs
+
+    const std::string filename;
 
 public:
-	struct Pos
-	{
-		unsigned int line = 0;
-		unsigned int col = 0;
+    // Struct to represent position of a character
+    struct Pos
+    {
+        unsigned int line = 0;
+        unsigned int col = 0;
 
-		std::string ToString();
-	};
+        std::string ToString();
+    };
 
-	JSONSource(std::string filename);
+    JSONSource(std::string filename);				// Read and trim source file
+    Pos GetSymbolSourcePosition(size_t trimmedPos);	// Iterate the file to find position
 
-	Pos GetSymbolSourcePosition(size_t trimmedPos);
+    // Return an initial JSONString, with whole trimmed string and offset of zero
+    const JSONString GetString() { return const JSONString(trimmedStr, this); }
+    std::string GetFilename() { return filename; }
+};
+
+// Augmented string type to support finding source position
+class JSONString : public std::string
+{
+    size_t offset;		// Offset from beginning of the source string
+    JSONSource* source;	// Class has no ownership of that pointer.
+                        // Thus the default constructor will do fine
+
+    // Instance of JSONString can only be created via:
+    //	1) JSONSource::GetString()	- Initial string with offset 0
+    //	2) JSONString::substr(..)	- All subsequent substrings
+    //
+    // It is a point of design that JSON strings are to be created only via substrings.
+    // It ensures that any JSON string is an actual fragment of trimmed source JSON file.
+    friend class JSONSource;
+    JSONString(std::string str, JSONSource* source, size_t offset = 0)
+        : std::string(str), source(source), offset(offset) { }
+public:
+
+    // Override substring method to increase offset
+    const JSONString substr(size_t _Off, size_t _Count)
+    {
+        return const JSONString(std::string::substr(_Off, _Count), source, offset += _Off);
+    }
+
+    // Access source position easily without having to care 
+    JSONSource::Pos GetSourcePos(size_t _Off = 0)
+    {
+        source->GetSymbolSourcePosition(offset + _Off);
+    }
+
+    // By creation, instance of JSONString cannot contain nullptr JSONSource pointer.
+    JSONSource* GetSource() { return source; }
+
+    void PrintSyntaxMsg(std::string errorText, int msgType = 0, size_t _Off = 0);
+
+    // Scan string at the beginning, bounded by \".
+    std::string ScanString(size_t& _Pos);
+    JSONString ScanSyntax(size_t& _Pos);
 };
--- src/json_parser.cpp
+++ src/json_parser.cpp
@@ -4,148 +4,270 @@
 #include <iostream>
 
 // Helper recursive function, assumes unprocessed strings
-JSON::JSONNode* resolve_json(std::string body);
+JSON::JSONNode* resolve_json(JSONString body);
 
 JSONSource::Pos JSONSource::GetSymbolSourcePosition(size_t trimmedPos)
 {
-	std::cout << "Symbol: " << trimmedStr.at(trimmedPos) << std::endl;
+    std::cout << "Symbol: " << trimmedStr.at(trimmedPos) << std::endl;
 
-	size_t sourceOffset = 0;	// Counts all symbols except newlines
-	size_t trimmedOffset = 0;	// Counts all symbols except whitespaces, tabs and newlines
-	size_t lineOffset = 0;		// Counts all symbols up to last newline, according to source
-	size_t line = 1;			// Number of whole lines
+    size_t sourceOffset = 0;	// Counts all symbols except newlines
+    size_t trimmedOffset = 0;	// Counts all symbols except whitespaces, tabs and newlines
+    size_t lineOffset = 0;		// Counts all symbols up to last newline, according to source
+    size_t line = 1;			// Number of whole lines
 
-	for (char& c : sourceStr)
-	{
-		if (trimmedOffset == trimmedPos + 1) break;
+    for (char& c : sourceStr)
+    {
+        if (trimmedOffset == trimmedPos + 1) break;
 
-		sourceOffset++;
+        sourceOffset++;
 
-		switch (c)
-		{
-		case ' ':
-		case '\t':
-			break;
-		case '\n':
-			line++;
-			lineOffset = sourceOffset;
-			break;
-		default:
-			trimmedOffset++;
-		}
+        switch (c)
+        {
+        case ' ':
+        case '\t':
+            break;
+        case '\n':
+            line++;
+            lineOffset = sourceOffset;
+            break;
+        default:
+            trimmedOffset++;
+        }
 
-	}
+    }
 
-	Pos query;
-	query.line = line;
-	query.col = sourceOffset - lineOffset;
+    Pos query;
+    query.line = line;
+    query.col = sourceOffset - lineOffset;
 
-	return query;
+    return query;
 }
 
 std::string JSONSource::Pos::ToString()
 {
-	std::string str = "(";
-	str += std::to_string(line);
-	str += ";";
-	str += std::to_string(col);
-	str += ")";
-	return str;
+    std::string str = "(";
+    str += std::to_string(line);
+    str += ";";
+    str += std::to_string(col);
+    str += ")";
+    return str;
 }
 
-JSONSource::JSONSource(std::string filename)
+JSONSource::JSONSource(std::string filename) : filename(filename)
 {
-	sourceStr = utilstr::ReadFromFile(filename);
+    sourceStr = utilstr::ReadFromFile(filename);
 
-	// Remove all spaces, tabs and newlines from the string
-	trimmedStr = sourceStr;
-	utilstr::ReplaceAllChars(trimmedStr, " \n\t", "");
+    // Remove all spaces, tabs and newlines from the string
+    trimmedStr = sourceStr;
+    utilstr::ReplaceAllChars(trimmedStr, " \n\t", "");
 }
 
-JSON JSON::ReadFromFile(const std::string& filename)
+JSON::JSON(const std::string& filename)
 {
-	return JSON::ReadFromString(utilstr::ReadFromFile(filename));
+    jsonSource = new JSONSource(filename);
+    JSONString source = jsonSource->GetString();
+
+    // Check for empty input
+    if (source.size() == 0)
+    {
+        std::string errorMsg =  "JSON file does not exist or is empty. ";
+        errorMsg +=             "An empty JSON instance is created.";
+        source.PrintSyntaxMsg(errorMsg, SYNTAX_MSG_TYPE_WARNING);
+
+        return;
+    }
+    
+    // Check first important condition - global space must be an object.
+    if (!utilstr::BeginsAndEndsWith(source, '{', '}'))
+    {
+        std::string errorMsg =  "JSON file does not contain an object. ";
+        errorMsg +=             "Correct format of the file would be: \"{..}\". ";
+        errorMsg +=             "Empty JSON object returned.";
+        source.PrintSyntaxMsg(errorMsg);
+
+        return;
+    }
+
+    // Here, by the condition above, we are certain that the global space
+    // is in fact a JSON object.
+    globalSpace = static_cast<JSONObject*>(resolve_json(source));
 }
 
-JSON JSON::ReadFromString(std::string source)
+void JSONString::PrintSyntaxMsg(std::string errorText, int msgType = 0, size_t _Off = 0)
 {
-	
+    // [ERROR] test1.json:2 - "X expected."
+    std::string msg;
 
-	// Check for empty input
-	if (source.size() == 0)
-	{
-		std::string errorMsg = "[WARNING] JSON file/string does not exist or is empty. ";
-		errorMsg += "An empty JSON instance is created.";
-		std::cerr << errorMsg << std::endl;
+    switch (msgType)
+    {
+    case 0:
+        msg = "[ERROR]";
+        break;
+    case 1:
+        msg = "[WARNING]";
+        break;
+    default:
+        msg = "[MESSAGE]";
+    }
 
-		return JSON();
-	}
-	std::cout << source << std::endl;
-	// Check first important condition - global space must be an object.
-	if (!utilstr::BeginsAndEndsWith(source, '{', '}'))
-	{
-		std::string errorMsg = "[ERROR] JSON file/string does not contain an object. ";
-		errorMsg += "Correct format of the file/string would be: \"{..}\". ";
-		errorMsg += "Empty JSON object returned.";
-		std::cerr << errorMsg << std::endl;
+    msg += " ";
+    msg += source->GetFilename();
+    msg += ":";
+    msg += std::to_string(GetSourcePos(_Off).line);
+    msg += " - ";
+    msg += errorText;
 
-		return JSON();
-	}
-
-	// Here, by the condition above, we are certain that the global space
-	// is in fact a JSON object.
-	JSON json;
-	json.globalSpace = static_cast<JSONObject*>(resolve_json(source));
-	
-	return json;
+    std::cerr << msg << std::endl;
 }
 
-JSON::JSONNode* resolve_json(std::string body)
+// Input: " \"TEXT\".. "
+// Output: TEXT, with
+//  \\ -> backslash
+//  \n -> newline
+//  \t -> tab
+//  \" -> "
+// Ignores anything after closing '"', returns position of
+// first character after it.
+// Invalid string: return "".
+std::string JSONString::ScanString(size_t& _Pos)
+{
+    if (at(0) != '"')
+    {
+        PrintSyntaxMsg("'\"' expected.");
+        return "";
+    }
+
+    bool escape = false;
+    bool closingFound = false;
+    std::string str;
+
+    size_t counter = 0;
+    for (char& c : *this)
+    {
+        counter++;
+
+        if (counter == 1) continue;
+
+        // If this character follows \, it is an escape sequence
+        if (escape)
+        {
+            escape = false;
+            switch (c)
+            {
+            case '\\':
+                str += '\\';
+                break;
+            case 'n':
+                str += '\n';
+                break;
+            case 't':
+                str += '\t';
+                break;
+            default:
+                PrintSyntaxMsg("Valid escape sequence expected.", SYNTAX_MSG_TYPE_WARNING, counter);
+            }
+            continue;
+        }
+        
+        if (c == '\\')
+        {
+            escape = true;
+            continue;
+        }
+
+        // Closing '"' found!
+        if (c == '"')
+        {
+            closingFound = true;
+            break;
+        }
+
+        // Any other character
+        str += c;
+    }
+
+    // Analyze why loop was teminated
+    if (!closingFound)
+    {
+        PrintSyntaxMsg("'\"' expected.", SYNTAX_MSG_TYPE_ERROR, counter - 1);
+        return "";
+    }
+    _Pos = counter;
+    return str;
+}
+
+JSON::JSONNode* resolve_json(JSONString body)
 {	
-	// Example JSON:
-	// {"menu":{"id":"file","value":"File"}}
+    // Example JSON:
+    // {"menu":{"id":"file","value":"File"}}
 
-	// A JSON object
-	if (utilstr::BeginsAndEndsWith(body, '{', '}'))
-	{
-		JSON::JSONObject* object = new JSON::JSONObject;
-		body = utilstr::TrimOneChar(body);
-		// "menu":{"id":"file","value":"File"}
-		
-		// Iterate through "id": value pairs
-		while (true)
-		{
-			size_t beginIdentifier, endIdentifier;
-			size_t beginBody, endBody;
+    // A JSON object
+    if (utilstr::BeginsAndEndsWith(body, '{', '}'))
+    {
+        // Create new JSON object and expose its members
+        JSON::JSONObject* object = new JSON::JSONObject;
+        body = body.substr(1, body.size() - 2);
 
-			if (body.at(0) != '"')
-			{
-				std::string errorMsg = "[ERROR] '\"' expected at";
-			}
-			
-		}
+        // "menu":{"id":"file","value":"File"}
+        
+        // Iterate through "id": value pairs
+        while (true)
+        {
+            // Expect '"'
+            if (body.at(0) != '"')
+            {
+                body.PrintSyntaxMsg("'\"' expected.");
+                break;
+            }
 
-		return object;
-	}
+            // Find non-escaped '"'
+            size_t counter = 0;
+            bool escape = false;
+            std::string id;
+            for (char& c : body)
+            {
+                if (counter == 0)
+                {
+                    counter++;
+                    continue;       // We are looking for closing '"'
+                }
 
-	// A JSON list
-	else if (utilstr::BeginsAndEndsWith(body, '[', ']'))
-	{
-		std::cout << "List not implemented yet." << std::endl;
-	}
+                if (escape)
+                {
+                    switch (c)
+                    {
+                    
+                                                
+                    }
+                }
+                if (c == '\\') escape = true;
+                
+                if (c == '"') break;
+                counter++;
+            }
+        }
 
-	// A string literal
-	else if (utilstr::BeginsAndEndsWith(body, '"'))
-	{
-		return new JSON::JSONLiteral<std::string>(utilstr::TrimOneChar(body),
-			JSON::JSON_NODE_TYPE::JSON_NODE_TYPE_LITERAL_STRING);
-	}
+        return object;
+    }
 
-	// Numeric literal or invalid
-	else
-	{
+    // A JSON list
+    else if (utilstr::BeginsAndEndsWith(body, '[', ']'))
+    {
+        std::cout << "List not implemented yet." << std::endl;
+    }
 
-	}
+    // A string literal
+    else if (utilstr::BeginsAndEndsWith(body, '"'))
+    {
+        return new JSON::JSONLiteral<std::string>(utilstr::TrimOneChar(body),
+            JSON::JSON_NODE_TYPE::JSON_NODE_TYPE_LITERAL_STRING);
+    }
 
-	return new JSON::JSONObject();
+    // Numeric literal or invalid
+    else
+    {
+
+    }
+
+    return new JSON::JSONObject();
 }
